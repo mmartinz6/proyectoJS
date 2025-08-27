c9a3cc50b98801c9450f898709db146529a6b9b4
mmartinz6 <madmartinez@gmail.com>
2025-08-27 13:54:17 -0600

    update

--- public/js/login.js
+++ public/js/login.js
@@ -1,2 +1,24 @@
-import { getUsers, postUsers } from "../services/servicesUsers.js";
+import { getRegistro } from "../services/servicesRegistro.js";
+
+const idUser = document.getElementById("idUser");
+const passUser = document.getElementById("passUser");
+const btnLogin = document.getElementById("btnLogin");
+const loginMensajeError = document.getElementById("loginMensajeError");
+
+btnLogin.addEventListener("click", async function () {
+    const usuarios = await getRegistro();
+
+    for (let index = 0; index < usuarios.length; index++) {
+        if (
+            usuarios[index].userName === idUser.value.trim() &&
+            usuarios[index].contrasenha === passUser.value.trim()
+        ) {
+            //Si hay una concidencia inicia sesión
+            window.location.href = "../pages/solicitudesEstudiantes.html";
+            return;
+        }
+    }
+
+    loginMensajeError.classList.remove("oculto");
+});
 
