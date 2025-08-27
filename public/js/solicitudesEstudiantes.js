aeeabe8cca61f1122918b9727b320fd998f9701b
mmartinz6 <madmartinez@gmail.com>
2025-08-27 15:08:45 -0600

    solicitud estudiantes

---
+++ public/js/solicitudesEstudiantes.js
@@ -0,0 +1,6 @@
+
+const nombreUsuario = document.getElementById("nombreUsuario");
+const sede = document.getElementById("sede");
+const fechaSalida = document.getElementById("fechaSalida");
+const codigoPc = document.getElementById("codigoPc");
+const condiciones = document.getElementById("condiciones");
